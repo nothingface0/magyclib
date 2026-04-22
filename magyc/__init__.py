4ae6a58a48eecba576d386213570928a1d75fa3d
Dimitris Papagiannis <d.papag.a@gmail.com>
2026-04-22 18:19:31 +0200

    Remove unneeded benchmark method

--- magyc/__init__.py
+++ magyc/__init__.py
@@ -2,7 +2,6 @@
 from .benchmark_methods import (
     ellipsoid_fit,
     ellipsoid_fit_fang,
-    magfactor3,
     sar_aid,
     sar_kf,
     sar_ls,
@@ -25,7 +24,6 @@ __all__ = ["ellipsoid_fit", "ellipsoid_fit_fang"]
 __all__ += ["sphere_fit"]
 __all__ += ["twostep_hi", "twostep_hsi"]
 __all__ += ["sar_aid", "sar_kf", "sar_ls"]
-__all__ += ["magfactor3"]
 __all__ += ["create_synthetic_dataset"]
 __all__ += ["hsi_calibration_validation", "pds_geodesic_distance"]
 __all__ += ["joe_hdg_error_std", "joe_pos_error", "joe_hdg_error_violin", "ellipsoid_plot", "magfield_data_plot"]
